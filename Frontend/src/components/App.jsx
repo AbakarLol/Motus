fb8ced499fe437ffd1f70652b6ba13beea578d1d
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-08-19 16:04:34 +0100

    add reset option

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -2,6 +2,7 @@ import Footer from "./Footer"
 import Header from "./Header"
 import Board from "./Board"
 import Field from "./Field"
+import Button from "./Button"
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Board />
       <Field />
+      <Button />
       <Footer />
     </div>
     
---
+++ Frontend/src/components/Button.jsx
@@ -0,0 +1,9 @@
+export default function Button(){
+    return(
+        <div>
+            <button className="bg-blue-400 hover:bg-blue-700 text-white text-2xl font-bold py-2 px-4 rounded">
+                Recommencer
+            </button>
+        </div>
+    )
+}
