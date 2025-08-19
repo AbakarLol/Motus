32f25b9824471e75582915c8f6c487c70c4848bc
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-08-19 16:13:18 +0100

    change the style

--- Frontend/src/components/Button.jsx
+++ Frontend/src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button(){
     return(
         <div>
-            <button className="bg-blue-400 hover:bg-blue-700 text-white text-2xl font-bold py-2 px-4 rounded">
+            <button className="text-black-400 bg-amber-50 shadow-xl/40 hover:shadow-2xl text-2xl font-bold py-2 px-4 rounded">
                 Recommencer
             </button>
         </div>
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header(){
     return(
         <div className="">
-            <h1 className="font-mono text-3xl text-amber-50 " >Motus</h1>
+            <h1>Mô Mô Motus </h1>
             <div className="text-amber-50" >
                 <p>Attempt : 6</p>
             </div>
