fab7ef2d5dc05b999fda9afa7735e4696f38505b
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-08-22 14:40:00 +0100

    add letters inputs

--- Frontend/src/components/Field.jsx
+++ Frontend/src/components/Field.jsx
@@ -1,7 +1,12 @@
 export default function Field(){
     return(
         
-        <div className="card md:h-1/2 md:w-150 w-full h-1/3">
+        <div className="card lg:h-1/2 md:w-150 w-full  h-1/3">
+            <input className="letter-input" type="text" maxLength="1"  autoFocus />
+            <input className="letter-input" type="text" maxLength="1"   />
+            <input className="letter-input" type="text" maxLength="1"   />
+            <input className="letter-input" type="text" maxLength="1"   />
+
         
         </div>
     )
