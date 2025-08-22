0784c5438b7a6590a572dee02fb8d44fa46bba50
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-08-22 15:39:52 +0100

    Introduce useRef to deal with focus problem

--- Frontend/src/components/Field.jsx
+++ Frontend/src/components/Field.jsx
@@ -1,8 +1,18 @@
+import { useRef, useEffect } from "react"
+
 export default function Field(){
+
+    const inputRef = useRef();
+
+    useEffect(() => {
+    console.log(inputRef)
+    inputRef.current.focus();
+    }, [] )
+
     return(
         
         <div className="card lg:h-1/2 md:w-150 w-full  h-1/3">
-            <input className="letter-input" type="text" maxLength="1"  autoFocus />
+            <input ref={inputRef} className="letter-input" type="text" maxLength="1" />
             <input className="letter-input" type="text" maxLength="1"   />
             <input className="letter-input" type="text" maxLength="1"   />
             <input className="letter-input" type="text" maxLength="1"   />
