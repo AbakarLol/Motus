7a424f9c4e232b8e4d037c89e8ceb8529c4129bf
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-08-25 13:36:31 +0100

    add words generating api

--- Frontend/src/components/Field.jsx
+++ Frontend/src/components/Field.jsx
@@ -1,100 +1,31 @@
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
+import axios from 'axios';
 
 export default function Field(){
-
+    const [words, setWords] = useState([]);
     const inputRef = useRef();
 
+    async function fetchWords(){  
+        try{
+            const response = (await axios.get("https://trouve-mot.fr/api/size/4/10"));
+            const result = response.data
+            const adaptedResult = result.map(word => word.name.toUpperCase())
+            console.log(adaptedResult)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
     useEffect(() => {
-    console.log(inputRef)
-    inputRef.current.focus();
+        console.log(inputRef)
+        inputRef.current.focus();
+        fetchWords();
     }, [] )
 
     return(
         
         <div className="grid grid-cols-10 grid-center card lg:h-1/2 md:w-150 w-full  h-1/3">
             <input ref={inputRef} className="letter-input" type="text" maxLength="1" />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-                        <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-                        <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-                        <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-            <input className="letter-input" type="text" maxLength="1"   />
-       
         </div>
     )
 }
