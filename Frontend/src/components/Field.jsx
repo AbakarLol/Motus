b546735818b86f9d1ff5bbb7057c471617f7fb83
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-08-26 13:58:45 +0100

    Increment auto focus

--- Frontend/src/components/Field.jsx
+++ Frontend/src/components/Field.jsx
@@ -3,11 +3,12 @@ import axios from 'axios';
 
 export default function Field(){
     const [words, setWords] = useState([]);
-    const inputRef = useRef();
+    const [inputValue, setInputValue] = useState("");
+    const inputRef = useRef([]);
 
     async function fetchWords(){  
         try{
-            const response = await axios.get("https://trouve-mot.fr/api/size/4/10");
+            const response = await axios.get("https://trouve-mot.fr/api/size/4/4");
             const result = response.data
             const adaptedResult = result.map(word => word.name.toUpperCase())
             console.log(adaptedResult)
@@ -19,15 +20,46 @@ export default function Field(){
 
     useEffect(() => {
         console.log(inputRef)
-        inputRef.current.focus();
         fetchWords();
         console.log("my words: " + words)
+        
     }, [] )
+
+    function handleChange(event){
+        const [, inputIndex] = event.target.name.split("-");
+        let refIndex = parseInt(inputIndex, 10);
+        console.log(event.target.name)
+        setInputValue(event.target.value);
+        console.log(inputValue)
+
+        if(refIndex < 3){
+            inputRef.current[refIndex+1].focus()
+
+        }else{
+            // const endOfIndex = document.querySelector(`Input[name="code-${refIndex}]"`);
+            // endOfIndex.blur()
+            inputRef.current[3].focus()
+
+        }
+
+    }
 
     return(
         
-        <div className="grid grid-cols-10 grid-center card lg:h-1/2 md:w-150 w-full  h-1/3">
-            <input ref={inputRef} className="letter-input" type="text" maxLength="1" />
+        <div className="card lg:h-1/2 md:w-150 w-full  h-1/3">
+            {
+                words.map((letter, index) => 
+                <input 
+                    ref={(ref) => inputRef.current.push(ref)} 
+                    className="letter-input" 
+                    type="text" 
+                    maxLength="1"
+                    key={index}
+                    name= {`code-${index}`}
+                    onChange={handleChange}
+                />
+                 )
+            }
         </div>
     )
 }
