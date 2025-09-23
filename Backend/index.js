5a158a1468897482f3fb0a99344cd27287f3dc7d
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-09-23 15:40:31 +0100

    initiate backend sevices

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import env from "dotenv"            
+
 
 const app = express();
-const port = 300;
+
+env.config()
+
+const port = process.env.BACKEND_PORT;
 
 app.listen(port, ()=>{
     console.log(`Server is working ${port}`);
