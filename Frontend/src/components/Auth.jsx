91f24150d432699d9d2e7cda8ddcc97f5e5f220f
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-09-25 15:13:11 +0100

    add sign up

--- Frontend/src/components/Auth.jsx
+++ Frontend/src/components/Auth.jsx
@@ -4,8 +4,11 @@ import axios from 'axios';
 export default function Auth(){
     const [user, setUser] = useState({
         username : "",
-        password : ""
-    })
+        password : "",
+        passwordConfirmation : ""
+    });
+
+    const [exist, setExist] = useState(true);
 
 function handleChange(event){
    const {name, value} = event.target;
@@ -18,10 +21,14 @@ async function handleClick(){
     console.log(user);
 }
 
+async function handleSigningUP() {
+    console.log(user)
+}
+
 
     return(
-        <div>
-            <div className="gradient card sm:w-110 h-110 w-80 flex flex-col justify-between items-start px-3 sm:px-5 py-20 sm:rounded-2xl rounded-xl" >
+        <div className="flex justify-center items-center w-full ">
+            <div className="gradient card sm:w-110 h-130 w-80 flex flex-col justify-arround items-start px-3 sm:px-5 py-20 sm:rounded-2xl rounded-xl" >
                 
                 <div className="w-full">
                     <label className="pl-1" htmlFor="">Username:</label>
@@ -31,11 +38,25 @@ async function handleClick(){
                     <label className="pl-1" htmlFor="">Password:</label>
                     <input onChange={handleChange} value={user.password} type="password" name="password" className="auth-input" />
                 </div>
+                {!exist && <div className="w-full">
+                    <label className="pl-1" htmlFor="">Confirm Password:</label>
+                    <input onChange={handleChange} value={user.passwordConfirmation} type="password" name="passwordConfirmation" className="auth-input" />
+                </div>}
+                {
+                exist?
                 <button
                 onClick={handleClick} 
                 className="bg-[#bc4e9c] hover:bg-[#fd2c72] w-full h-10 text-amber-50 rounded-md border-2 border-[#bc4e9c]"  >
                 Submit
                 </button>
+                :
+                <button
+                onClick={handleSigningUP} 
+                className="bg-[#bc4e9c] hover:bg-[#fd2c72] w-full h-10 text-amber-50 rounded-md border-2 border-[#bc4e9c]"  >
+                Sign Up
+                </button>
+                
+                }
                 
 
 
