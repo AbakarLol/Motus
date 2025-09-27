71c49e893a21b18a1dd9e39e1876697b2a6552f6
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2025-09-27 20:29:49 +0100

    add db user

--- Backend/index.js
+++ Backend/index.js
@@ -5,17 +5,21 @@ import bcrypt from "bcrypt"
 
 
 const app = express();
-const db = new pg.Client({
-    database:'',
-    port:'',
-    host:"",
-    user:'',
-    password: ""
-});
 
 env.config()
 
+const db = new pg.Client({
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST ,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+db.connect();
+
 const port = process.env.BACKEND_PORT;
+
 
 app.listen(port, ()=>{
     console.log(`Server is working ${port}`);
